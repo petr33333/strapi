7fddd3b2dfae97112e0e611d1ff709defe328072
petr33333 <148391671+petr33333@users.noreply.github.com>
2025-07-10 00:05:04 +0200

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
-  url: env('PUBLIC_URL') + '/admin',
+  url: env('URL') + '/admin',
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
