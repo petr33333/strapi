6fb4f60ae51e9487f1634dda9a1ca3649a6f1229
petr33333 <148391671+petr33333@users.noreply.github.com>
2025-07-10 11:12:52 +0200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,34 @@
-module.exports = [
+// config/middlewares.js
+module.exports = ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            // strip https:// or http:// off your CF_PUBLIC_ACCESS_URL
+            "pub-3c584da98a5e432eaa179800ab7fefd4.r2.dev"
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'pub-3c584da98a5e432eaa179800ab7fefd4.r2.dev',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
